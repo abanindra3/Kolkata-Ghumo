35c140b8076250bf40f22e09626e2ced44be3247
abanindra3 <122215052+abanindra3@users.noreply.github.com>
2024-08-17 20:58:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const axios = require('axios'); 
-
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
